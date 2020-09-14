36dba2a2006742e2804f4c58e6b3d53155e1c3eb
wangqinghe95 <675072584@qq.com>
2020-09-14 22:11:06 +0800

    [C/C++]区间合并

---
+++ Q56_合并区间.cpp
@@ -0,0 +1,30 @@
+/*
+如果 merged 为空， 或者最后一堆区域的右值小于新区域的左值，就将新区域附加到二维数组最后
+否则就需要判断最后一个区域的右值和合新区域的右值谁大取谁，替换旧区域的的右值
+*/
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if (intervals.size() == 0)
+        {
+            return {};
+        }
+
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> merged;
+        for (int i = 0; i < intervals.size(); ++i)
+        {
+            int L = intervals[i][0];
+            int R = intervals[i][1];
+            if (!merged.size() || merged.back()[1] < L)
+            {
+                merged.push_back({L,R});
+            }
+            else
+            {
+                merged.back()[1] = max(merged.back()[1], R);
+            }
+        }
+        return merged;
+    }
+};
