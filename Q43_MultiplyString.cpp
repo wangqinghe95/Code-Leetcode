f11f9455eb07d80a78e1fbb075b953bca5e3ce14
wangqinghe95 <675072584@qq.com>
2020-10-31 10:01:03 +0800

    [C/C++]multiply two string by simulating multiply

--- Q43_MultiplyString.cpp
+++ Q43_MultiplyString.cpp
@@ -76,6 +76,44 @@ string multiply(string num1, string num2)
 /*
 multiply two string by simulating multiply 
 */
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if (num1 == "0"|| num2 == "0")
+        {
+            return "0";
+        }
+
+        int m = num1.size();
+        int n = num2.size();
+        vector<int> res(m+n);
+        for (int i = m-1; i >= 0; --i)
+        {
+            int x = num1[i] - '0';
+            for (int j = n-1; j >= 0; --j)
+            {
+                int y = num2[j] - '0';
+                res[i + j + 1] += x * y;
+            }
+        }
+
+        for (int i = m + n - 1; i > 0; i--)
+        {
+            res[i-1] += res[i] / 10;
+            res[i] %= 10;
+        }
+
+        int index = res[0] == 0 ? 1 : 0;
+        string ans;
+        while (index < m + n )
+        {
+            ans.push_back(res[index] += '0');
+            index++;
+        }
+
+        return ans;
+    }
+};
 
 /*
 multiply two string by simulating multiply -- optimized version  
