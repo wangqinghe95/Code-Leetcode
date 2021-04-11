64b01b140f5e32aa53d08898abb959b11f1330e2
wangqinghe95 <675072584@qq.com>
2021-04-11 09:58:20 +0800

    [C/C++]二叉搜索树的插入

---
+++ Q701_Insert into a Binary Search Tree.cpp
@@ -0,0 +1,38 @@
+/*
+二叉搜索树的插入
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* newNode(int val){
+        TreeNode* node = new TreeNode;
+        node->val = val;
+        node->left = nullptr;
+        node->right = nullptr;
+        return node;
+    }
+
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if (root == nullptr){
+            root = newNode(val);
+        }
+        else if (root->val > val){
+            root->left = insertIntoBST(root->left, val);
+        }
+        else{
+            root->right = insertIntoBST(root->right, val);
+        }
+        return root;
+    }
+};
